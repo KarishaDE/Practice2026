31bfa2c00f022e459d36ee3e0eaef526136a4ce7
KarishaDE <Karizay9@gmail.com>
2026-07-15 08:22:43 +0300

    Выполнено задание 05: рефлексия в C#

--- task05/ClassAnalyzer.cs
+++ task05/ClassAnalyzer.cs
@@ -1,13 +1,10 @@
-﻿using System;
 using System.Reflection;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace task05;
 
 public class ClassAnalyzer
 {
-    private Type _type;
+    private readonly Type _type;
 
     public ClassAnalyzer(Type type)
     {
@@ -15,34 +12,34 @@ public class ClassAnalyzer
     }
 
     public IEnumerable<string> GetPublicMethods()
-    {
-        return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
-                    .Select(method => method.Name);
-    }
+        => _type
+            .GetMethods(BindingFlags.Public | BindingFlags.Instance |
+                        BindingFlags.Static | BindingFlags.DeclaredOnly)
+            .Select(method => method.Name);
 
     public IEnumerable<string> GetMethodParams(string methodName)
-    {
-        var method = _type.GetMethod(methodName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-        if (method is null)
-            return Enumerable.Empty<string>();
-
-        return method.GetParameters().Select(param => $"{param.ParameterType.Name} {param.Name}");
-    }
+        => _type
+            .GetMethods(BindingFlags.Public | BindingFlags.Instance |
+                        BindingFlags.Static | BindingFlags.DeclaredOnly)
+            .Where(method => method.Name == methodName)
+            .SelectMany(method => method
+                .GetParameters()
+                .Select(parameter => parameter.Name ?? string.Empty)
+                .Append(method.ReturnType.Name));
 
     public IEnumerable<string> GetAllFields()
-    {
-        return _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                    .Select(field => field.Name);
-    }
+        => _type
+            .GetFields(BindingFlags.Public | BindingFlags.NonPublic |
+                       BindingFlags.Instance | BindingFlags.Static |
+                       BindingFlags.DeclaredOnly)
+            .Select(field => field.Name);
 
     public IEnumerable<string> GetProperties()
-    {
-        return _type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                    .Select(property => property.Name);
-    }
+        => _type
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance |
+                           BindingFlags.Static | BindingFlags.DeclaredOnly)
+            .Select(property => property.Name);
 
     public bool HasAttribute<T>() where T : Attribute
-    {
-        return _type.GetCustomAttribute<T>() is not null;
-    }
+        => _type.GetCustomAttribute<T>() is not null;
 }
