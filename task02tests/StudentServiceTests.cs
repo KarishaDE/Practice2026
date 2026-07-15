1412f4f2ec443b87265207c541544f5d4723ee06
KarishaDE <Karizay9@gmail.com>
2026-07-15 07:54:50 +0300

    Выполнено задание 02: основы LINQ

--- task02tests/StudentServiceTests.cs
+++ task02tests/StudentServiceTests.cs
@@ -1,6 +1,3 @@
-using NUnit.Framework;
-using System.Collections.Generic;
-using System.Linq;
 using task02;
 
 namespace task02tests;
@@ -8,11 +5,11 @@ namespace task02tests;
 [TestFixture]
 public class StudentServiceTests
 {
-    private List<Student> _testStudents;
-    private StudentService _service;
+    private List<Student> _testStudents = null!;
+    private StudentService _service = null!;
 
     [SetUp]
-    public void Setup()
+    public void SetUp()
     {
         _testStudents = new List<Student>
         {
@@ -20,47 +17,53 @@ public class StudentServiceTests
             new() { Name = "Анна", Faculty = "ФИТ", Grades = new List<int> { 3, 4, 3 } },
             new() { Name = "Петр", Faculty = "Экономика", Grades = new List<int> { 5, 5, 5 } }
         };
+
         _service = new StudentService(_testStudents);
     }
 
     [Test]
-    public void GetStudentsByFaculty_ShouldReturnCorrectStudents()
+    public void GetStudentsByFaculty_ReturnsCorrectStudents()
     {
         var result = _service.GetStudentsByFaculty("ФИТ").ToList();
-        Assert.AreEqual(2, result.Count);
-        Assert.IsTrue(result.All(s => s.Faculty == "ФИТ"));
+
+        Assert.That(result.Count, Is.EqualTo(2));
+        Assert.That(result.All(student => student.Faculty == "ФИТ"), Is.True);
     }
 
     [Test]
-    public void GetStudentsWithMinAverageGrade_ShouldReturnCorrectStudents()
+    public void GetStudentsWithMinAverageGrade_ReturnsCorrectStudents()
     {
         var result = _service.GetStudentsWithMinAverageGrade(4).ToList();
-        Assert.AreEqual(2, result.Count);
-        Assert.IsTrue(result.All(s => s.Grades.Average() >= 4));
+
+        Assert.That(result.Count, Is.EqualTo(2));
+        Assert.That(result.All(student => student.Grades.Average() >= 4), Is.True);
     }
 
     [Test]
-    public void GetStudentsOrderedByName_ShouldReturnCorrectOrder()
+    public void GetStudentsOrderedByName_ReturnsStudentsInAlphabeticalOrder()
     {
         var result = _service.GetStudentsOrderedByName().ToList();
-        Assert.AreEqual("Анна", result[0].Name);
-        Assert.AreEqual("Иван", result[1].Name);
-        Assert.AreEqual("Петр", result[2].Name);
+
+        Assert.That(result[0].Name, Is.EqualTo("Анна"));
+        Assert.That(result[1].Name, Is.EqualTo("Иван"));
+        Assert.That(result[2].Name, Is.EqualTo("Петр"));
     }
 
     [Test]
-    public void GroupStudentsByFaculty_ShouldReturnCorrectGroups()
+    public void GroupStudentsByFaculty_ReturnsCorrectGroups()
     {
         var result = _service.GroupStudentsByFaculty();
-        Assert.AreEqual(2, result.Count);
-        Assert.AreEqual(2, result["ФИТ"].Count());
-        Assert.AreEqual(1, result["Экономика"].Count());
+
+        Assert.That(result.Count, Is.EqualTo(2));
+        Assert.That(result["ФИТ"].Count(), Is.EqualTo(2));
+        Assert.That(result["Экономика"].Count(), Is.EqualTo(1));
     }
 
     [Test]
-    public void GetFacultyWithHighestAverageGrade_ShouldReturnCorrectFaculty()
+    public void GetFacultyWithHighestAverageGrade_ReturnsCorrectFaculty()
     {
         var result = _service.GetFacultyWithHighestAverageGrade();
-        Assert.AreEqual("Экономика", result);
+
+        Assert.That(result, Is.EqualTo("Экономика"));
     }
 }
